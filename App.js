ca0d9dc46155cb9e2bad017c28fe1de98ebcbb02
Gant <GantMan@gmail.com>
2019-01-19 11:27:52 -0600

    it works

--- App.js
+++ App.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react'
 import { Button, StyleSheet, View, Text } from 'react-native'
-
+import { getHex } from 'react-native-pixel-color'
 import { SketchCanvas } from '@terrylinla/react-native-sketch-canvas'
+const modelJSON = require('./trained-model')
+const brain = require('brain.js')
+let net = new brain.NeuralNetwork()
+net.fromJSON(modelJSON)
 
 export default class example extends Component {
+  maxScore(obj) {
+    let maxKey = 0;
+    let maxValue = 0;
+
+    Object.entries(obj).forEach(entry => {
+      const value = entry[1];
+      if (value > maxValue) {
+        maxValue = value;
+        maxKey = parseInt(entry[0], 10);
+      }
+    });
+
+    return maxKey;
+  }
+
+  grabPixels = () => {
+    this.canvas.getBase64('jpg', false, true, false, false, (err, result) => {
+      getHex(`data:image/jpg;base64,${result}`, { x: 0, y: 1 })
+        .then(color => {
+          console.log(color)
+          const detection = net.run(color);
+          alert(this.maxScore(detection));
+        })
+        .catch(console.error)
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <View style={{ flex: 1 }}>
           <SketchCanvas
-            ref="canvas"
+            ref={ref => (this.canvas = ref)}
             style={{
               width: 280,
               height: 280,
@@ -17,11 +48,11 @@ export default class example extends Component {
               marginTop: 30,
               borderWidth: 1
             }}
-            strokeColor={'red'}
-            strokeWidth={20}
-            onStrokeEnd={() => true}
+            strokeColor={'#FF0000'}
+            strokeWidth={50}
+            onStrokeEnd={this.grabPixels}
           />
-          <Button title="Clear" onPress={() => this.refs.canvas.clear()} />
+          <Button title="Clear" onPress={() => this.canvas.clear()} />
         </View>
       </View>
     )
